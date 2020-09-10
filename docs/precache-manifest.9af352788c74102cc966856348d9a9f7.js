9a803e9c7a3fdf80f2afdbfc529f09b503bbf20d
Camilo Valencia <cromalcocamilovro@gmail.com>
2020-09-10 14:58:11 -0500

    Despliegue de la app

---
+++ docs/precache-manifest.9af352788c74102cc966856348d9a9f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13767c8780a694539da0377dc6a85479",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04edb06faaccf543010c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5bcee0420ebd8b954328",
+    "url": "/static/js/2.fda69a0b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fda69a0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04edb06faaccf543010c",
+    "url": "/static/js/main.47d11efa.chunk.js"
+  },
+  {
+    "revision": "4b2213e085cf53028354",
+    "url": "/static/js/runtime-main.68b67fc5.js"
+  }
+]);
